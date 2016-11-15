52bffbc26407dc04247b36ebebbf69ace9bcdce9
slindquist3 <slindquist3@gmail.com>
2016-11-15 15:38:12 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 //Code React element here
+
+
+let li1 = React.createElement('li', {}, "JavaScript")
+let li2 = React.createElement('li', {}, "React")
+let li3 = React.createElement('li', {}, "Movies")
+let li4 = React.createElement('li', {}, "Ice cream")
+let ul = React.createElement('ul', {className: "me_interests"}, [li1, li2, li3, li4])
+let h1 = React.createElement('h1', {}, "An Awesome Person")
+let p = React.createElement('p', {}, "Who is learning React")
+let meInReact = React.createElement('div', {className: "me"}, [h1, p, ul])
+
+ReactDOM.render(form1, document.getElementById('content'))
